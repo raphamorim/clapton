3cc3a83eb574f41cc91e7c74d248faeb0269f3dc
Raphael Amorim <rapha850@gmail.com>
2017-08-07 14:03:49 -0300

    update lock and update support

--- README.md
+++ README.md
@@ -4,6 +4,14 @@
 
 ![Example](assets/images/clapton.png)
 
+## Supported Formats
+
+Format       |HLS|MP4|MP3|WEBM| DASH | RTMP | JPG/PNG/GIF |
+-------------|---|---|---|----|------|------|-------------|
+Beta Clapton | ✔ | ✔ | ✔ |  ✔ | ![dash](http://flv.io/external3.png) | ![rtmp](http://flv.io/external3.png) | ✔
+
+![rtmp](http://flv.io/external3.png) means that the support is made by an external plugin.
+
 ## Credits
 
 Created by [@raphamundi](https://twitter.com/raphamundi).
--- src/clapton.js
+++ src/clapton.js
@@ -1,14 +1,14 @@
 const remote = require('electron').remote
 const shell = require('electron').shell
 
-const playerElement = document.querySelector("#player")
-const containerElement = document.querySelector(".container")
-const openFileElement = document.querySelector("#open-file")
-const openTorrentElement = document.querySelector("#open-torrent")
-const versionElement = document.querySelector("#version")
+const playerElement = document.querySelector('#player')
+const containerElement = document.querySelector('.container')
+const openFileElement = document.querySelector('#open-file')
+const openTorrentElement = document.querySelector('#open-torrent')
+const versionElement = document.querySelector('#version')
 
-const linkTwitterElement = document.querySelector("#link-twitter")
-const linkContributeElement = document.querySelector("#link-contribute")
+const linkTwitterElement = document.querySelector('#link-twitter')
+const linkContributeElement = document.querySelector('#link-contribute')
 
 const version = require('./package.json').version
 versionElement.textContent = version
@@ -32,8 +32,14 @@ function play(filePath) {
 
 function openVideoFile() {
   remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
+    // TODO: 'mkv', 'avi'
     filters: [
-      { name: 'Movies', extensions: [ 'mkv', 'avi', 'mp4' ] }
+      {
+        name: 'Movies',
+        extensions: [
+          'mp4', 'webm', 'hls', 'mp3', 'jpg', 'png', 'gif'
+        ]
+      }
     ],
     properties: [ 'openFile' ]
   }, (fileNames) => {
