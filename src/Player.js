b46c706f19016503ffa90c336b15f7246b2fe1b9
raphamorim <rapha850@gmail.com>
2017-08-19 21:45:20 -0300

    removing iframes in favour of HTMLVideo

--- src/Player.js
+++ src/Player.js
@@ -1,30 +1,36 @@
 class Player {
   constructor(config) {
     this.config = config
+    this._source = null
     this._options = config
   }
 
   escapeHandler(ev) {
     if (e.keyCode === 27)
       console.log(11)
-
   }
 
   play() {
-    const { parent, source } = this.config
-    const iframe = document.createElement('iframe')
-    iframe.width = "100%"
-    iframe.height = "100%"
-    iframe.src = source
+    const {
+      parent,
+      source
+    } = this.config
 
-    if (iframe.addEventListener)
-      iframe.addEventListener('keydown', this.escapeHandler, true)
-
-    parent.appendChild(iframe)
+    const videoElement = document.createElement('video')
+    parent.appendChild(videoElement)
+    console.log(parent)
+    const sourceElement = document.createElement('source')
+    videoElement.style.width="100%"
+    videoElement.controls = true
+    videoElement.setAttribute('src', source)
+    videoElement.insertBefore(sourceElement, videoElement.children[0])
+    this._source = videoElement
+    videoElement.load()
+    videoElement.play()
   }
 
   pause() {
-    console.log(this.config)
+    this._source.pause()
   }
 
   resize() {
